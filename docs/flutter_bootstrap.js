40fc987784225283fee2999031dcd65fd6d4bbaf
Hendril Mendes <hendrilmendes2015@gmail.com>
2024-06-27 08:39:08 -0400

    Ajustado href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2039237689"
+    serviceWorkerVersion: "1714722545"
   }
 });
