352a32fbebddbfc381696b38ee96776512d1d556
Hendril Mendes <hendrilmendes2015@gmail.com>
2024-07-03 16:43:22 -0400

    Ajuste no menu de configurações

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,3 +1,4 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:projectx/theme/theme.dart';
@@ -9,9 +10,35 @@ import 'package:projectx/widgets/settings/support.dart';
 import 'package:projectx/widgets/settings/theme.dart';
 import 'package:projectx/widgets/settings/update.dart';
 import 'package:provider/provider.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _isAndroid12 = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndroidVersion();
+  }
+
+  Future<void> _checkAndroidVersion() async {
+    if (Platform.isAndroid) {
+      final deviceInfo = DeviceInfoPlugin();
+      final androidInfo = await deviceInfo.androidInfo;
+      final version = androidInfo.version.sdkInt;
+      setState(() {
+        _isAndroid12 = version >= 31;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +53,7 @@ class SettingsScreen extends StatelessWidget {
           buildCategoryHeader(
               AppLocalizations.of(context)!.interface, Icons.palette_outlined),
           ThemeSettings(themeModel: themeModel),
-          const DynamicColorsSettings(),
+          if (_isAndroid12) const DynamicColorsSettings(),
           buildCategoryHeader(
               AppLocalizations.of(context)!.outhers, Icons.more_horiz_outlined),
           buildUpdateSettings(context),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import dynamic_color
 import flutter_tts
 import geolocator_apple
@@ -16,6 +17,7 @@ import speech_to_text_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
